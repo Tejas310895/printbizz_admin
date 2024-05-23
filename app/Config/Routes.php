f0ad05c2776ba743eb1c6dafba2404ab15be67f0
Tejas310895 <43029698+Tejas310895@users.noreply.github.com>
2024-05-23 23:04:55 +0530

    changes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,14 +6,15 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 
-
-$routes->get('/', 'MainController::index');
-$routes->match(['get', 'post'], 'orders-index', 'OrdersController::index');
-$routes->match(['get', 'post'], 'products-index', 'ProductsController::index');
-$routes->match(['get', 'post'], 'products-itemnary', 'ProductsController::itemnary');
-$routes->match(['get', 'post'], 'customers-index', 'CustomersController::index');
-$routes->match(['get', 'post'], 'institutions-index', 'InstitutionsController::index');
-$routes->match(['get', 'post'], 'partners-index', 'PartnersController::index');
+$routes->group('', ['filter' => ['session', \App\Filters\AccessFilter::class]], static function ($routes) {
+    $routes->get('/', 'MainController::index');
+    $routes->match(['get', 'post'], 'orders-index', 'OrdersController::index');
+    $routes->match(['get', 'post'], 'products-index', 'ProductsController::index');
+    $routes->match(['get', 'post'], 'products-itemnary', 'ProductsController::itemnary');
+    $routes->match(['get', 'post'], 'customers-index', 'CustomersController::index');
+    $routes->match(['get', 'post'], 'institutions-index', 'InstitutionsController::index');
+    $routes->match(['get', 'post'], 'partners-index', 'PartnersController::index');
+});
 
 service('auth')->routes($routes);
 
--- app/Views/includes/sidebar.php
+++ app/Views/includes/sidebar.php
@@ -127,6 +127,6 @@
                 </ul>
             </li>
             <li><a href="javascript:void(0);" class="js-right-sidebar" title="Setting"><i class="zmdi zmdi-settings zmdi-hc-spin"></i></a></li>
-            <li><a href="sign-in.html" class="mega-menu" title="Sign Out"><i class="zmdi zmdi-power"></i></a></li>
+            <li><a href="logout" class="mega-menu" title="Sign Out"><i class="zmdi zmdi-power"></i></a></li>
         </ul>
     </div>
