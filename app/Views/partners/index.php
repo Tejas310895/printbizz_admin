53bc5f06999003ce13b41c64b685cf4b633912c3
Tejas310895 <43029698+Tejas310895@users.noreply.github.com>
2024-05-30 22:26:42 +0530

    changes

--- app/Views/partners/index.php
+++ app/Views/partners/index.php
@@ -3,10 +3,10 @@
         <div class="block-header">
             <div class="row">
                 <div class="col-lg-7 col-md-6 col-sm-12">
-                    <h2>Institutions</h2>
+                    <h2>Partners</h2>
                     <ul class="breadcrumb">
                         <li class="breadcrumb-item"><a href="index.html"><i class="zmdi zmdi-home"></i> Home</a></li>
-                        <li class="breadcrumb-item"><a href="javascript:void(0);">Institutions</a></li>
+                        <li class="breadcrumb-item"><a href="javascript:void(0);">Partners</a></li>
                         <li class="breadcrumb-item active">List</li>
                     </ul>
                     <button class="btn btn-primary btn-icon mobile_menu" type="button"><i class="zmdi zmdi-sort-amount-desc"></i></button>
